7e877aa1516e1088e6d72832acdb3c0a91d194c5
Yustinia <yustinia@protonmail.com>
2026-05-08 19:56:03 +0800

    temp final

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func main() {
+	language := validateArgs()
+
 	cwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	createTemplate(cwd, templates["python"])
+	template, ok := templates[language]
+	if !ok {
+		fmt.Printf("Unknown language: %v", language)
+		os.Exit(1)
+	}
+
+	createTemplate(cwd, template)
+}
+
+func validateArgs() string {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: gmk <language>")
+		os.Exit(1)
+	}
+
+	return os.Args[1]
 }
 
 func createTemplate(cwd string, template []string) {
